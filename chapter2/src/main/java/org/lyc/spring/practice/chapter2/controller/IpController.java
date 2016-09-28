65ab9c75aea1a26ee0ef12ae358cf5a4f25e97a8
admin <lyc751@hotmail.com>
2016-09-28 17:00:11 +0800

    chapter2

--- chapter2/src/main/java/org/lyc/spring/practice/chapter2/controller/IpController.java
+++ chapter2/src/main/java/org/lyc/spring/practice/chapter2/controller/IpController.java
@@ -1,18 +1,28 @@
 package org.lyc.spring.practice.chapter2.controller;
 
+import org.lyc.spring.practice.chapter2.common.CommonUtil;
 import org.lyc.spring.practice.chapter2.service.IpInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by admin on 2016/9/28.
  */
-@RestController
+@RestController("/ip")
 public class IpController {
 
     @Autowired
     private IpInfoService ipInfoService;
 
-
+    @RequestMapping(value = "/getipinfo",method = {RequestMethod.POST,RequestMethod.GET})
+    public String getIpInfo(@RequestParam("ip") String ip){
+        if (!CommonUtil.isIp(ip)){
+            return "{\"Bad Ip!\"}";
+        }else {
+            return ipInfoService.getIpInfo(ip);
+        }
+    }
 }
